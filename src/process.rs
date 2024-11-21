ce738ffb495e454aac38323b277ae405bb0915a9
Ly-GGboy <li1980303503@gmail.com>
2024-11-21 23:34:48 +0800

    refactor: 重构process.rs 兼容多种csv格式

--- src/process.rs
+++ src/process.rs
@@ -1,25 +1,15 @@
-use serde::{Deserialize, Serialize};
 use csv::Reader;
 use anyhow::Result;
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit_number: u8,
-}
+use serde_json::Value;
 
 pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let mut ret: Vec<Player> = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let player: Player = result?;
-        ret.push(player);
+    let mut ret= Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let player = result?;
+        let record = headers.iter().zip(player.iter()).collect::<Value>();
+        ret.push(record);
     }
     let json = serde_json::to_string_pretty(&ret)?;
     std::fs::write(output, json)?;
